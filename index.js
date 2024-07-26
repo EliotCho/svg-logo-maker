31513082008ba879913bc6e7fd065f97a85ebbab
Eliot Cho <eliotcho99@gmail.com>
2024-07-25 23:58:18 -0400

    starting js for index.js, simple shapes created in shapes.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const svgGen = ({ name, nameColor, shape, shapeColor }) =>
+  // Template for the svg logo file
+  ``;
+
+// Inquirer prompt where the inputs are collected
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your logo (max 3 characters)?",
+    },
+    {
+      // color can be hex, rgb, or color name
+      type: "input",
+      name: "nameColor",
+      message: "What color do you want your logo name to be?",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "What shape would you like to use?",
+      choices: ["circle", "square", "triangle"],
+    },
+    {
+      // color can be hex, rgb, or color name
+      type: "input",
+      name: "shapeColor",
+      message: "What color do you want your logo shape to be?",
+    },
+  ])
+
+  .then((answers) => {
+    console.log(answers);
+
+    // generate the README file is created as a variable
+    const svgLogo = svgGen(answers);
+    let fileName = shape + logo + ".svg";
+    // write the README file
+    fs.writeFile("readmeGenerated.md", svgLogo, (err) =>
+      err ? console.error(err) : console.log("SVG Logo Generated!")
+    );
+  });
